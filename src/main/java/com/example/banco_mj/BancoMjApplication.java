5db3d6d305d7032d989542e1c2efa016afe7f011
Jhonatan Santos <48073565+jrsantos1@users.noreply.github.com>
2022-09-10 18:58:03 -0300

    first commit

---
+++ src/main/java/com/example/banco_mj/BancoMjApplication.java
@@ -0,0 +1,13 @@
+package com.example.banco_mj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoMjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoMjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/banco_mj/BancoMjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banco_mj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoMjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
